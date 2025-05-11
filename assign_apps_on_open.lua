5d7cc2f4aaea00539ef97fc84971d6ec8f8da3cc
Metal Gabe <gfl.wrk@gmail.com>
2025-05-10 22:07:25 -0700

    chore(main): update `apps_set` iteration logging; create foundation for individual `workspaces`/desktop files;

--- assign_apps_on_open.lua
+++ assign_apps_on_open.lua
@@ -5,20 +5,38 @@ local function process(apps_set)
 	local window_name = get_window_name()
 	window_name = string.lower(window_name)
 
-	print("----------")
-	print(app_name)
-	print(window_name)
-	print("----------")
+	print()
+	print("=====================[ START ]=====================")
+	print("Current App: " .. app_name)
+	print("Current Window: " .. window_name)
+	print("======================[ END ]======================")
+	print()
 
 	for _, data in ipairs(apps_set) do
-		print("Processing:", string.upper(data.name))
+		if data.name == "" or data.workspace == "" then
+			print("No data name or workspace provided, ignoring...")
+		else
+			print("Processing: " .. string.upper(data.name))
 
-		if string.match(app_name, data.name) or string.match(window_name, data.name) then
-			print('Moving "%s" to workspace #%s...', data.name, data.workspace)
-			set_window_workspace(data.workspace)
-			set_window_position(data.position[1], data.position[2])
-			maximize()
+			if string.match(app_name, data.name) or string.match(window_name, data.name) then
+				print("Moving '" .. data.name .. "' to workspace " .. data.workspace .. "...")
+				set_window_workspace(data.workspace)
+				set_window_position2(data.position[1], data.position[2])
+				maximize()
+			else
+				print(
+					"name: '"
+						.. data.name
+						.. "' did not match app_name: '"
+						.. (app_name or "UNKNOWN")
+						.. "' or window_name: '"
+						.. (window_name or "UNKNOWN")
+						.. "'"
+				)
+			end
 		end
+
+		print()
 	end
 end
 
--- devilspie2.lua
+++ devilspie2.lua
@@ -1,15 +1,88 @@
+-- DOWNLOAD LINK: https://git.savannah.gnu.org/cgit/devilspie2.git/
+-- ===========================================================
+-- SETTING THE PACKAGE PATH FOR MODULES TO BE FOUND
+-- ===========================================================
+local config_dir = "/home/nneurall/.config/devilspie2"
+package.path = package.path .. ";" .. config_dir .. "/?.lua" .. ";" .. config_dir .. "/workspaces/?.lua"
+
+-- ===========================================================
+-- IMPORTED MODULES
+-- ===========================================================
 local assign_apps_on_open = require("assign_apps_on_open")
 
+-- position: x, y
+
 local apps_set = {
+	-- ==========[ WORKSPACE # 02 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 03 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 04 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 05 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 06 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 07 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 08 ]==========
 	{
 		name = "discord",
-		workspace = 8,
 		position = { 1920, 0 },
+		workspace = 8,
 	},
 	{
 		name = "slack",
-		workspace = 8,
 		position = { 0, 0 },
+		workspace = 8,
+	},
+	-- ==========[ WORKSPACE # 09 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 10 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 11 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
+	},
+	-- ==========[ WORKSPACE # 12 ]==========
+	{
+		name = "",
+		position = { 0, 0 },
+		workspace = 1,
 	},
 }
 
