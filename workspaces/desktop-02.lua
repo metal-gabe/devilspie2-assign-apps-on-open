849c4f0c006e22eda9b5686b54256021ce2731db
Metal Gabe <gfl.wrk@gmail.com>
2025-05-19 09:47:44 -0700

    chore(main): move `brave` to desk 12; add `screen` vars for dryness;

--- workspaces/desktop-02.lua
+++ workspaces/desktop-02.lua
@@ -1,15 +1,13 @@
 -- position: x, y, idx
 -- `idx` is the number of the monitor
+local screen_01_left = { 0, 864, 1 }
+local screen_02_right = { 4608, 0, 2 }
+local screen_03_center = { 2560, 0, 3 }
 
 local desk_02_apps = {
    {
-      name = "brave",
-      position = { 0, 864, 1 },
-      workspace = 2,
-   },
-   {
       name = "unityhub-bin",
-      position = { 2560, 0, 3 },
+      position = screen_03_center,
       workspace = 2,
    },
 }
--- workspaces/desktop-12.lua
+++ workspaces/desktop-12.lua
@@ -1,10 +1,18 @@
 -- position: x, y, idx
 -- `idx` is the number of the monitor
+local screen_01_left = { 0, 864, 1 }
+local screen_02_right = { 4608, 0, 2 }
+local screen_03_center = { 2560, 0, 3 }
 
 local desk_12_apps = {
    {
       name = "ai tools",
-      position = { 4608, 0, 2 },
+      position = screen_02_right,
+      workspace = 12,
+   },
+   {
+      name = "brave",
+      position = screen_03_center,
       workspace = 12,
    },
 }
