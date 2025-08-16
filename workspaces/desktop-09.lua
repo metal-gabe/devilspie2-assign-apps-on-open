4f5e1c839600071fd1fb900cdfcbd1b5841244d9
Metal Gabe <gfl.wrk@gmail.com>
2025-08-16 10:12:05 -0700

    chore(main): remove JetBrains IDEs from desk 9 w/ note;

--- workspaces/desktop-09.lua
+++ workspaces/desktop-09.lua
@@ -4,6 +4,10 @@ local screen_01_left = { 0, 864, 1 }
 local screen_02_right = { 4608, 0, 2 }
 local screen_03_center = { 2560, 0, 3 }
 
+-- NOTE :: DO NOT ADD JetBrains IDEs HERE!!!
+-- DP will auto-resize all of their pop-up windows (e.g. autocomplete suggestions)
+-- So if you're setting things to full screen, every pop-up will cover the rest of the IDE
+
 local desk_09_apps = {
    {
       name = "code",
@@ -13,16 +17,6 @@ local desk_09_apps = {
    {
       name = "codium",
       position = screen_02_right,
-      workspace = 9,
-   },
-   {
-      name = "pycharm",
-      position = screen_03_center,
-      workspace = 9,
-   },
-   {
-      name = "rustrover",
-      position = screen_03_center,
       workspace = 9,
    },
    {
